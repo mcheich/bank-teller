9aa0c9a3f54050ba6906efd3c4159240c4169e5a
mcheich <mcheich@gmail.com>
2020-01-17 09:47:28 -0500

    Junit Tests and Classes and methods built for Account and Bank

--- src/main/java/bankTeller/Bank.java
+++ src/main/java/bankTeller/Bank.java
@@ -1,10 +1,63 @@
 package bankTeller;
 
-public class Bank {
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
-	public void addAccount(String accountNumber, String AccounType, double balance) {
+public class Bank {
+	
+	private Map<String ,Account> accounts = new HashMap<String, Account>();
+
+	/**
+	 * Add Account to accounts Map.
+	 * Calls the constructor of the account Class
+	 * @param accountNumber
+	 * @param AccounType
+	 * @param balance
+	 */
+	public void openNewAccount(String accountNumber, String accountType, double balance) {
 		
+		Account accountToAdd = new Account(accountNumber, accountType, balance);
+		accounts.put(accountToAdd.getAccountNumber(), accountToAdd);
 		
 	}
+	
+/**
+ * Returns a Collection of all the accounts in the bank. 
+ * @return Collection<Account> All the accounts in the bank
+ */
+	public Collection<Account> getAllAccounts() {
+		return accounts.values();
+	}
+	
+/**
+ * Removes an account from accounts map.
+ * @param accountNumber
+ */
+	public void closeAccount(String accountNumber) {
+		accounts.remove(accountNumber);
+		
+	}
+
+	/**
+	 * Retrieves a single account object from the bank.
+	 * @param accountNumber
+	 * @return Account object
+	 */
+public Account getAccount(String accountNumber) {
+	
+	return accounts.get(accountNumber);
+}
+
+/**
+ * Adds an Account object to the bank.
+ * ????? MIKE NOTE: Not sure if the naming I use for this method is OK.
+ * @param account
+ */
+public void putAccount(Account account) {
+	
+	accounts.put(account.getAccountNumber(), account);
+
+}
 
 }
--- src/test/java/bankTeller/BankTest.java
+++ src/test/java/bankTeller/BankTest.java
@@ -10,7 +10,83 @@ public class BankTest {
 		//Arrange
 		Bank bank = new Bank();
 		//Act
-		bank.addAccount("1234","checking",100.01);
+		bank.openNewAccount("1234","checking",100.01);
+		//Assert
+		assertEquals(1, bank.getAllAccounts().size());
 	}
+	
+	@Test
+	public void closeAccountRemovesAccount() {
+		//Arrange
+		Bank bank = new Bank();
+		//Act
+		bank.openNewAccount("1234", "checking", 100.01);
+		bank.openNewAccount("5678", "savings", 10000);
+		bank.closeAccount("1234");
+		//Assert
+		assertEquals(1, bank.getAllAccounts().size());
+	}
+	
+	@Test
+	public void getAccountReturnsCorrectAccountObject() {
+		//Arrange
+		Bank bank = new Bank();
+		Account account = new Account("9999", "investment", 100000); 
+		//Act
+		bank.openNewAccount("1234", "checking", 100.01);
+		bank.openNewAccount("5678", "savings", 10000);
+		bank.putAccount(account);
+		Account result = bank.getAccount("9999");
+		//Assert
+		assertEquals(result, account);
+	}
+	
+	@Test
+	public void depositAdds100p11ToAccountBalance() {
+		//Arrange
+		Bank bank = new Bank();
+		//Act
+		bank.openNewAccount("1234", "checking", 100.01);
+		bank.openNewAccount("5678", "savings", 10000);
+		bank.getAccount("1234").deposit(100.11);
+		//Assert
+		assertEquals(200.12, bank.getAccount("1234").checkAccountBalance(), 0.001); //last value is precision
+	}
+	
+	@Test
+	public void withdrawlremoves100p50FromAccountBalance() {
+		//Arrange
+		Bank bank = new Bank();
+		//Act
+		bank.openNewAccount("1234", "checking", 100.99);
+		bank.openNewAccount("5678", "savings", 10000);
+		bank.getAccount("1234").withdrawl(100.50);
+		//Assert
+		assertEquals(0.49, bank.getAccount("1234").checkAccountBalance(), 0.001); //last value is precision
+	}
+	
+	@Test
+	public void withdrawlremovesDoesNotAllowOverDraft() {
+		//Arrange
+		Bank bank = new Bank();
+		//Act
+		bank.openNewAccount("1234", "checking", 100.99);
+		bank.openNewAccount("5678", "savings", 10000);
+		bank.getAccount("1234").withdrawl(1000);
+		//Assert
+		assertEquals(100.99, bank.getAccount("1234").checkAccountBalance(), 0.001); //last value is precision
+	}
+	
+	@Test
+	public void OverDraftAttemptReturnsOverDraftMessage() {
+		//Arrange
+		Bank bank = new Bank();
+		//Act
+		bank.openNewAccount("1234", "checking", 100.99);
+		bank.openNewAccount("5678", "savings", 10000);
+		//Assert
+		assertEquals("Withdral amount exceeds balance, change withdrawl amount.", bank.getAccount("1234").withdrawl(1000));
+	}
+	
 
 }
